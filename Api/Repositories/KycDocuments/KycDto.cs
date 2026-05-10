11632bdbe6abce2d8967455587b9570f161f7bbd
MatYu <manalangmatthew13@gmail.com>
2026-05-11 00:06:19 +0800

    Create KycDto.cs

---
+++ Api/Repositories/KycDocuments/KycDto.cs
@@ -0,0 +1,17 @@
+﻿//KYC ID
+//Customer ID
+//IDTYPE enum {
+//Philsys
+//Passport
+    //Umid
+    //SSSId
+    //PostalId
+    //ProfessionalId
+    //DriversLicense
+    //TINId
+//}
+//Address
+//Full Name
+//Image ng Id
+//Passport
+//
