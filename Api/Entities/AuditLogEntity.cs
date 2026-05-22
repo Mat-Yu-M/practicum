e5f569aa9563b6bf2d5b77eee4171020a6514791
MatYu <manalangmatthew13@gmail.com>
2026-05-22 09:35:48 +0800

    Create AuditLogEntity.cs

---
+++ Api/Entities/AuditLogEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Api.Entities
+
+public sealed class AuditLogEntity
+{
+    public long Id { get; init; }
+    public string Action { get; init; } = null!;
+    public string PerformedBy { get; init; } = null!;
+    public DateTime PerformedAt { get; init; } = DateTime.UtcNow;
+    public string? Details { get; init; }
+}
