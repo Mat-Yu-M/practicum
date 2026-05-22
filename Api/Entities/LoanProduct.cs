c34765d1758826ce45e6d27d1ea042063ff21eca
MatYu <manalangmatthew13@gmail.com>
2026-05-22 09:33:09 +0800

    x

---
+++ Api/Entities/LoanProduct.cs
@@ -0,0 +1,14 @@
+﻿namespace Api.Entities;
+
+public sealed class LoanProduct
+{
+    public long Id { get; init; }
+    public required string Name { get; init; }
+    public required string Description { get; init; }
+    public decimal InterestRate { get; init; }
+    public decimal MinimumAmount { get; init; }
+    public decimal MaximumAmount { get; init; }
+    public int MinimumTermMonths { get; init; }
+    public int MaximumTermMonths { get; init; }
+    public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
+}
