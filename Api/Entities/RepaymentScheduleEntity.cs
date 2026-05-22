f7f44c7b24e5db6ad1c0a2e82723d716e7fcd610
MatYu <manalangmatthew13@gmail.com>
2026-05-23 00:36:03 +0800

    Create RepaymentScheduleEntity.cs

---
+++ Api/Entities/RepaymentScheduleEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace Api.Entities
+{
+    public class RepaymentScheduleEntity
+    {
+        public long id;
+        public long loanId;
+        public long userId;
+        public decimal amount;
+        public DateTime dueDate;
+    }
+}
