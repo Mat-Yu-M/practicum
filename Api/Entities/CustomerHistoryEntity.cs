30cdd1884f7294d08c549e70ba3870ba73f231f6
MatYu <manalangmatthew13@gmail.com>
2026-06-02 15:24:17 +0800

    new table

---
+++ Api/Entities/CustomerHistoryEntity.cs
@@ -0,0 +1,14 @@
+﻿using Api.Constants;
+
+public sealed record CustomerHistoryEntity
+{
+    public long Id { get; init; }
+    public long UserId { get; init; }
+    public long LoanId { get; init; }
+    public decimal LoanAmount { get; init; }
+    public CommonStatus Status { get; init; }
+    public long RepaymentScheduleId { get; init; }
+    public string? Action { get; init; }
+    public string? ApprovedBy { get; init; }
+    public DateTime ApprovedAt { get; init; } = DateTime.UtcNow;
+}
