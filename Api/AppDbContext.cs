8e1f2140421dcb1a8976e47b1325d108a4a6441b
MatYu <manalangmatthew13@gmail.com>
2026-06-08 11:10:22 +0800

    Update AppDbContext.cs

--- Api/AppDbContext.cs
+++ Api/AppDbContext.cs
@@ -7,9 +7,9 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(op
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
         base.OnModelCreating(modelBuilder);
     }
-    public virtual DbSet<UserEntity> Users { get; set; } = null;
-    public virtual DbSet<EmployeeEntity> Employees { get; set; } = null;
-    public virtual DbSet<KycEntity> Kyc { get; set; } = null;
-    public virtual DbSet<LoanEntity> Loan { get; set; } = null;
-    public virtual DbSet<LoanProductEntity> LoanProduct { get; set; } = null;
+    public virtual DbSet<UserEntity> Users { get; set; } 
+    public virtual DbSet<EmployeeEntity> Employees { get; set; } 
+    public virtual DbSet<KycEntity> Kyc { get; set; }
+    public virtual DbSet<LoanEntity> Loan { get; set; } 
+    public virtual DbSet<LoanProductEntity> LoanProduct { get; set; }
 }
