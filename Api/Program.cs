ef33c52319e077bd2d4100fecf641defeda1a46a
MatYu <manalangmatthew13@gmail.com>
2026-06-10 19:29:13 +0800

    fix

--- Api/Program.cs
+++ Api/Program.cs
@@ -5,7 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 
-builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("practicumdb")));
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
@@ -15,27 +16,35 @@ app.MapDefaultEndpoints();
 
 using (var scope = app.Services.CreateScope())
 {
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
+    var attempts = 0;
+    while (true)
+    {
+        try
+        {
+            var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            db.Database.Migrate();
+            break;
+        }
+        catch (Npgsql.NpgsqlException) when (attempts++ < 6)
+        {
+            Console.WriteLine($"DB not ready, retrying in 5s... (attempt {attempts}/6)");
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+        }
+    }
 }
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.MapScalarApiReference(options => 
-    {options.WithTitle("Practicum API")
-        .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient); }
-        );
-    
+    app.MapScalarApiReference(options =>
+        options.WithTitle("Practicum API")
+               .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient));
 }
-
-if (!app.Environment.IsDevelopment())
+else
 {
     app.UseHttpsRedirection();
 }
 
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
