fcfff35c84f7f016aaa9ece32d5fbd0dde7136a2
MatYu <manalangmatthew13@gmail.com>
2026-06-23 08:54:06 +0800

    Create test.cs

---
+++ Api/Entities/AuditLogs/test.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Api.Entities.AuditLogs
+{
+
+    public sealed record Root
+    {
+        [JsonPropertyName("ssa")]
+        public TestParent ssa { get; set; }
+        public sealed record TestParent
+        {
+            [JsonPropertyName("ssa")]
+            public string? ssa { get; set; }
+        }
+    }
+}
+
+
