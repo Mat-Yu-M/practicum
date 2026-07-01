a2d93383a60ffe0716ae178584a8e75a4d12d629
MatYu <manalangmatthew13@gmail.com>
2026-07-01 22:41:09 +0800

    LoanRequest

---
+++ Api/Controllers/Requests/LoanRequestController.cs
@@ -0,0 +1,73 @@
+﻿using Api.Constants;
+using Api.Entities.AuditLogs;
+using Api.Entities.LoanRequests;
+using Api.Repositories.LoanRequests;
+using Api.Services.AuditLogs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers.Requests;
+
+[ApiController]
+[Route("api/[Controller]")]
+public class LoanRequestController : ControllerBase
+{
+    private readonly ILoanRequestRepository _repository;
+    private readonly IAuditLogService _auditLog;
+
+    public LoanRequestController(ILoanRequestRepository repository, IAuditLogService auditLog)
+    {
+        _repository = repository;
+        _auditLog = auditLog;
+    }
+
+    [HttpPost("add-loan-request")]
+    public async Task<IActionResult> AddLoanRequest(LoanRequestRequest request)
+    {
+
+
+        var loanRequest = new AddLoanRequestDto
+        {
+            CustomerId = request.CustomerId,
+            Name = request.Name,
+            LoanProductId = request.LoanProductId,
+            LoanName = request.LoanName,
+            Status = CommonStatus.Pending,
+            RequestType = LoanRequestType.Add,
+            InterestRate = request.InterestRate,
+            Amount = request.Amount,
+            StartDate = request.StartDate,
+            EndDate = request.EndDate,
+            CreatedBy = request.CreatedBy,
+            CreatedDate = request.CreatedDateTime
+        };
+
+        var loanRequestResult = await _repository.AddAsync(loanRequest);
+
+        var response = new AuditLogResponse(AuditLogType.Add, "Successfully added Loan Request", $"Added {loanRequestResult.Id}");
+
+        await _auditLog.LogAsync(response);
+
+        return Created($"api/LoanRequest/{loanRequestResult.Id}", new
+        {
+            loanRequestResult.Id
+        });
+    }
+
+
+
+    [HttpDelete("reject-loan-request")]
+    public async Task<IActionResult> RejectLoanRequest(long id)
+    {
+
+        await _repository.DeleteAsync(id);
+
+        var response = new AuditLogResponse(AuditLogType.Reject, "Successfully Rejected Loan Request", $"Rejected {id}");
+
+        await _auditLog.LogAsync(response);
+
+        return Ok();
+
+    }
+
+}
+
---
+++ Api/Entities/LoanRequests/LoanRequestContracts.cs
@@ -0,0 +1,26 @@
+﻿using Api.Constants;
+
+namespace Api.Entities.LoanRequests
+{
+    public record LoanRequestRequest(
+    long CustomerId,
+    string Name,
+    long LoanProductId,
+    string LoanName,
+    decimal Amount,
+    decimal InterestRate,
+    CommonStatus Status,
+    LoanRequestType RequestType,
+    DateTime StartDate,
+    DateTime EndDate,
+    string CreatedBy,
+    DateTime CreatedDateTime
+    );
+
+    public record LoanRequestResponse(
+    string Name,
+    string LoanName,
+    string CreatedBy,
+    DateTime CreatedDateTime
+    );
+}
